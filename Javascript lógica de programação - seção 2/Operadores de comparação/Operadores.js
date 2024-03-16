3992dc4825d527aa75a45952691bed252741499b
Lianderdev <liandervinicius13@gmail.com>
2024-03-15 21:50:40 -0300

    Operadores ( comparação - lógio )

---
+++ Javascript lógica de programação - seção 2/Operadores de comparação/Operadores.js
@@ -0,0 +1,28 @@
+/*
+Operadores de comparação 
+> Maior que
+>= Maior ou igual que
+< Menor que
+>= Menor ou igual que
+== Igual (valor)
+=== Igualdade estrita (valor e tipo)
+!= Diferente (valor)
+!== Diferente estrito (valor e tipo)
+*/
+
+console.log(10 > 5)
+console.log(5 < 2)
+console.log(10 >= 10)
+console.log(4 <= 4)
+
+let num1 = 10
+let num2 = '10'
+let num3 = 10
+
+console.log(num1 === num2)
+
+console.log(num1 === num3)
+
+console.log(num1 !== num2)
+
+console.log(num1 !== num3)
---
+++ Javascript lógica de programação - seção 2/Operadores lógicos/Operadores-logicos.js
@@ -0,0 +1,17 @@
+/*
+Operadores Lógicos
+
+&& -> And -> E
+|| -> OU -> OU
+! -> NOT -> NÃO
+*/
+
+console.log('And = ',true && true) // retorna verdadeiro
+console.log('And = ',true && false) //  retorna falso
+
+console.log('OU =', true || true) //  retorna verdadeiro
+console.log('OU =', true || false) //  retorna verdadeiro
+console.log('OU =', false || false) //  retorna falso
+ 
+console.log('NOT = ', !true) //  retorna falso
+console.log('NOT = ', !false) //  retorna verdadeiro

4aa0047d529057e92ef82275ebef5d89752dccd8
Lianderdev <liandervinicius13@gmail.com>
2024-03-27 23:42:50 -0300

    while

---
+++ Javascript lógica de programação - seção 2/While/while.js
@@ -0,0 +1,10 @@
+// Estrutura de repetição While
+
+let controle = 0;
+let nome = 'liander';
+while ( controle < nome.length) {
+    console.log(nome[controle]);
+    controle++;
+}
+
+console.log('Segue a vida...')

bdcdc2c024a3e7cf45b378b3286217c636e694ff
Liander Souza <liandervinicius13@gmail.com>
2024-08-20 11:11:34 -0300

    trabalhando com os dados da api

--- Seção 7 - Javascript Assíncrono/rick and morty - API/script.js
+++ Seção 7 - Javascript Assíncrono/rick and morty - API/script.js
@@ -1,28 +1,53 @@
 // Consumindo a (API) do rick and morty
 
 const character = document.getElementById('characterId')
-const botao = document.getElementById('go')
+const button = document.getElementById('go')
+const reset = document.getElementById('reset')
 const content = document.getElementById('content')
 const img = document.getElementById('img')
 
 const url = 'https://rickandmortyapi.com/api/character'
 
-async function fetchApi(value){
+async function fetchApi(value) {
     const response = await fetch(`${url}/${value}`)
     const data = await response.json()
-    console.log(data)
     return data
 }
 
-const ep = []
+const keys = ['name', 'status', 'species', 'gender', 'origin', 'episode']
 
-botao.addEventListener('click', async (event) => {
+const buildResult = (result) => {
+    return keys.map((key) => document.getElementById(key))
+        .map((elem) => {
+            if (elem.checked && typeof (result[elem.name]) !== 'object') {
+                const p = document.createElement('p')
+                p.innerHTML = `${elem.name}: ${result[elem.name]}`
+                content.appendChild(p)
+            }
+        })
+}
+
+
+button.addEventListener('click', async (event) => {
     event.preventDefault()
+
+    if (character.value === '') {
+        return content.innerHTML = 'pfv - escolha um id'
+    }
+
     const result = await fetchApi(character.value)
-    content.textContent = `${JSON.stringify(result, undefined, 2)}`
-    img.src = result.image
-    ep.push(result.episode)
-    ep.map((ep) => {
-        console.log(ep)
-    })
+
+    if(content.firstChild === null) {
+        buildResult(result)
+        img.src = result.image
+    } else {
+        content.innerHTML = ''
+        buildResult(result)
+        img.src = result.image
+    }
+})
+
+reset.addEventListener('click', () => {
+    content.textContent = ''
+    img.src = ''
 })
